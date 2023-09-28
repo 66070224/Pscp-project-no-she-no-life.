85a37e64d3597714b829da3e744dec50b196c49a
66070224 <66070224@kmitl.ac.th>
2023-09-28 22:42:29 +0700

    v4

--- eieigame.py
+++ eieigame.py
@@ -10,14 +10,19 @@ frame = pygame.time.Clock()
 
 playerhealth = 100
 playerstamina = 100
+damage = 25
+
 stamina = True
 run = True
 monster = True
 game_buff = True
-hit = False
-immortal = 0
+buffspeed = False
+immortal = False
+immortal_time = 0
+
 buffrespawn = 0
-damage = 25
+buffspeed = False
+buffspeed_time = 0
 
 background_surface = pygame.Surface((width, height))
 background_surface.fill('White')
@@ -40,8 +45,12 @@ while True:
     key_input = pygame.key.get_pressed()
     screen_diplay.blit(background_surface, (0,0))
     if playerhealth > 0:
+
         step = 5
-        if hit:
+        
+        if buffspeed:
+            player_character.fill('Gold')
+        elif immortal:
             player_character.fill('Grey')
         else:
             player_character.fill('Green')
@@ -49,12 +58,8 @@ while True:
 
         if playerhealth < 100:
             player_health = pygame.Surface((playerhealth, 10))
-            player_health.fill('Green')
+            player_health.fill('Red')
             player_health_rect = player_health.get_rect(center = (player_rect.x+25, player_rect.y-20))
-            player_health_back = pygame.Surface((100, 10))
-            player_health_back.fill('Red')
-            player_health_back_rect = player_health_back.get_rect(center = (player_rect.x+25, player_rect.y-20))
-            screen_diplay.blit(player_health_back, player_health_back_rect)
             screen_diplay.blit(player_health, player_health_rect)
 
         if playerstamina < 100:
@@ -65,8 +70,10 @@ while True:
                 player_stamina.fill('Yellow')
             player_stamina_rect = player_stamina.get_rect(center = (player_rect.x+25, player_rect.y-30))
             screen_diplay.blit(player_stamina, player_stamina_rect)
-
-        if key_input[pygame.K_LSHIFT] and stamina:
+    
+        if buffspeed and (pygame.time.get_ticks() < buffspeed_time):
+            step *= 2
+        if key_input[pygame.K_LSHIFT] and stamina and not buffspeed:
             step *= 1.5
             playerstamina -= 0.5
             if playerstamina <= 5:
@@ -86,24 +93,27 @@ while True:
         if key_input[pygame.K_s] and player_rect.y <= height-75:
             player_rect.y += step
 
-
     if game_buff:
         screen_diplay.blit(buff, buff_rect)
-    if player_rect.colliderect(buff_rect) and (game_buff == True) and (playerhealth != 100):
+    if player_rect.colliderect(buff_rect) and (game_buff == True):
         game_buff = False
         playerhealth = 100
         buffrespawn = pygame.time.get_ticks()+5000
+        buffspeed_time = pygame.time.get_ticks()+1500
+        buffspeed = True
     if not game_buff and (pygame.time.get_ticks() >= buffrespawn):
         game_buff = True
+    if pygame.time.get_ticks() > buffspeed_time:
+        buffspeed = False
 
     if monster:
         screen_diplay.blit(monster_character, monster_rect)
-    if not player_rect.colliderect(monster_rect) and (pygame.time.get_ticks() >= immortal) and hit:
-        hit = False
-    if player_rect.colliderect(monster_rect) and (monster == True) and (hit == False):
+    if not player_rect.colliderect(monster_rect) and (pygame.time.get_ticks() >= immortal_time) and immortal:
+        immortal = False
+    if player_rect.colliderect(monster_rect) and (monster == True) and (immortal == False):
         playerhealth -= damage
-        hit = True
-        immortal = pygame.time.get_ticks()+1500
+        immortal = True
+        immortal_time = pygame.time.get_ticks()+1500
 
     pygame.display.update()
     frame.tick(75)
